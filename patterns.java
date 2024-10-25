b6d734f81c15482926812ff327630271016cabe7
Syed Athuallah R <syedathaullah786786@gmail.com>
2024-10-25 22:10:00 +0530

    Second patterns program

--- patterns.java
+++ patterns.java
@@ -5,17 +5,29 @@ public class patterns { //   *****
 //    *****
     public void  pattern1(int n) {
         for (int i = 0; i < n; i++) {
-           for(int j = 0; j< n; j++ ){
+           for(int j = 0; j<n; j++ ){
                System.out.print("*");
            }
            System.out.println();
         }
-        return ;
+    }
+//    *
+//    **
+//    ***
+//    ****
+//    *****
 
+    public void pattern2(int n) {
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
             patterns p = new patterns();
-            p.pattern1(5);
+            p.pattern2(5);
     }
 }
