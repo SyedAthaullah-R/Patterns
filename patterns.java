f898779a6f33ba564ae45610bf6d21d2200bbc43
Syed Athuallah R <syedathaullah786786@gmail.com>
2024-10-27 01:41:57 +0530

    Pattern  15

--- patterns.java
+++ patterns.java
@@ -283,10 +283,40 @@ public class patterns { //   *****
                 System.out.println();
             }
         }
+//Pattern 15
+//         *
+//        ***
+//       *****
+//      *******
+//     *********
+//      *******
+//       *****
+//        ***
+//         *
 
+    public void pattern15(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i +1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i < n; i++){
+            for (int j = 0 ; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0 ; j < 2 * (n - i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
             patterns p = new patterns();
-            p.pattern14(5);
+            p.pattern15(5);
     }
 }
