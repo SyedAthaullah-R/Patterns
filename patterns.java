e626c866196128cbbd633c038b987ea5198a1e1c
Syed Athuallah R <syedathaullah786786@gmail.com>
2024-10-27 00:48:55 +0530

    pattern 11 and pattern 12

--- patterns.java
+++ patterns.java
@@ -166,9 +166,67 @@ public class patterns { //   *****
             System.out.println();
         }
     }
+//    Pattern 11
+//         *
+//        * *
+//       *   *
+//      *     *
+//     *********
+
+    public void pattern11(int n) {
+        for(int i = 0; i < n; i++){
+            for (int j = i ; j< n; j++) {
+                System.out.print(" ");
+            }
+            if( i == 0 || i == n-1){
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    System.out.print("*");
+                }
+
+            }
+            else{
+                System.out.print("*");
+                for(int j = 0; j < 2 * i - 1 ; j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+//    Pattern 12
+// *********
+//  *     *
+//   *   *
+//    * *
+//     *
+
+    public void pattern12(int n) {
+        for( int i = 0; i < n ; i++){
+            for(int j = 0; j <= i; j++ ){
+                System.out.print(" ");
+            }
+            if (i == 0 || i == n-1){
+                for (int j = 0; j < 2 * (n - i) - 1; j++) {
+                    System.out.print("*");
+                }
+
+            }
+            else{
+                System.out.print("*");
+                for(int j = 0; j < 2 * (n - i) - 3 ; j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 
     public static void main(String[] args) {
             patterns p = new patterns();
-            p.pattern10(5);
+            p.pattern12(5);
     }
 }
