d10a851f9d06bfd7858f665279c90ebddbedc86e
Syed Athuallah R <syedathaullah786786@gmail.com>
2024-10-26 18:58:29 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -133,6 +133,11 @@ public class patterns { //   *****
             System.out.println();
         }
     }
+//     *
+//    ***
+//   *****
+//  *******
+// *********
     public void pattern9(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = i ; j < n; j++) {
