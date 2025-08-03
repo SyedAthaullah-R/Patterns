b1fc555764afe585588f17361b2505ef9ec28009
Syed Athuallah R <syedathaullah786786@gmail.com>
2025-08-03 12:51:26 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -488,9 +488,163 @@ public class patterns { //   *****
             System.out.println();
         }
     }
+    // Pattern 22
+    //     1
+    //    121
+    //   12321
+    //  1234321
+    // 123454321
+    public void pattern22(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n - 1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j <= i; j++){
+                System.out.print(j + 1);
+            }
+            for(int j = i - 1; j >= 0; j--){
+                System.out.print(j + 1);
+            }
+            System.out.println();
+        }
+    }
+    // Pattern 23
+    //     A
+    //    ABA
+    //   ABCBA
+    //  ABCDCBA
+    // ABCDEDCBA
+    public void pattern23(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n - 1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j <= i; j++){
+                System.out.print((char) (65 + j));
+            }
+            for(int j = i - 1; j >= 0; j--){
+                System.out.print((char) (65 + j));
+            }
+            System.out.println();
+        }
+    }
+    // Pattern 24
+    //     A
+    //    ABC
+    //   ABCDE
+    //  ABCDEFG
+    // ABCDEFGHI
+    //  ABCDEFG
+    //   ABCDE
+    //    ABC
+    //     A
+    public void pattern24(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n - 1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < 2 * i + 1; j++){
+                System.out.print((char) (65 + j));
+            }
+            System.out.println();
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= i - 1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < 2 * (n - i) - 1; j++){
+                System.out.print((char) (65 + j));
+            }
+            System.out.println();
+        }
+    }
+    // Pattern 25
+    // ***** *****
+    // ****   ****
+    // ***     ***
+    // **       **
+    // *         *
+    // **       **
+    // ***     ***
+    // ****   ****
+    // ***** *****
+    public void pattern25(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                System.out.print("*");
+            }
+            for(int j = 0; j < 2 * i + 1; j++){
+                System.out.print(" ");
+            }
+            for(int j = i; j < n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            for(int j = 0; j < 2 * (n - i) - 1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    // Pattern 26
+    //     *
+    //    * *
+    //   *   *
+    //  *     *
+    // *       *
+    //  *     *
+    //   *   *
+    //    * *
+    //     *
+    public void pattern26(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n - 1; j++){
+                System.out.print(" ");
+            }
+            if(i == 0){
+                for(int j = 0; j <= i; j++){
+                    System.out.print("*");
+                }
+            }
+            else{
+                System.out.print("*");
+                for(int j = 0; j < 2 * i - 1; j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= i - 1; j++){
+                System.out.print(" ");
+            }
+            if(i == n - 1){
+                for(int j = i; j < n; j++){
+                    System.out.print("*");
+                }
+            }
+            else{
+                System.out.print("*");
+                for(int j = 0; j < 2 * (n - i) - 3; j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
             patterns p = new patterns();
-            p.pattern21(5);
+            p.pattern26(5);
     }
 }
